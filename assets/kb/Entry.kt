6758ef29278c11e2af36406ddcbaf11110103936
Seddik Bouyakoub <sir.seddik@gmail.com>
2026-01-23 13:21:22 +0100

    Initial working voice assistant with branding and UI

---
+++ assets/kb/Entry.kt
@@ -0,0 +1,7 @@
+data class Entry(
+    val ar: String,
+    val en: String,
+    val fr: String,
+    val id: String,
+    val tags: List<String>
+)
---
+++ assets/kb/academy_kb.json.kt
@@ -0,0 +1,4 @@
+data class `academy_kb.json`(
+    val entries: List<Entry>,
+    val languages: List<String>
+)
